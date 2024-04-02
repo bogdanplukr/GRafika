233c9201380d0f31fd7dd5c07773153ebe6b21b5
zakhb <zakhbogdan@gmail.com>
2024-04-02 08:58:55 +0200

    Dodaj pliki projektów.

---
+++ GRafika/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿
+namespace GRafika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Paint4d = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button10 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Paint4d)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Paint4d
+            // 
+            this.Paint4d.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Paint4d.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Paint4d.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Paint4d.Image = ((System.Drawing.Image)(resources.GetObject("Paint4d.Image")));
+            this.Paint4d.Location = new System.Drawing.Point(11, 11);
+            this.Paint4d.Margin = new System.Windows.Forms.Padding(2);
+            this.Paint4d.Name = "Paint4d";
+            this.Paint4d.Size = new System.Drawing.Size(484, 309);
+            this.Paint4d.TabIndex = 0;
+            this.Paint4d.TabStop = false;
+            this.Paint4d.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Paint4d_MouseMove);
+            this.Paint4d.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Paint4d_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Window;
+            this.button2.Location = new System.Drawing.Point(4, 28);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(2, 5);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 19);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "kolore";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(64, 28);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(78, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(4, 5);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 19);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "białe tło";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(11, 28);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(56, 19);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(11, 324);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(253, 81);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.trackBar1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(245, 55);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(151, 34);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(56, 19);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(151, 18);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(56, 19);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(151, 4);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(56, 19);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(245, 55);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(269, 348);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(370, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(420, 353);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(833, 12);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click_2);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(500, 11);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 173);
+            this.listBox1.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(727, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(761, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1070, 412);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.Paint4d);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.Paint4d)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Paint4d;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GRafika/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GRafika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int rozmiar = 4;
+        Bitmap bmp;
+
+        List<Point> listap = new List<Point>();
+        Brush colork = Brushes.Black;
+        private static IPEndPoint consumerEndPoint;
+        private static UdpClient udpClient = new UdpClient();
+        private void Paint4d_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Left)
+            {
+               
+                listap.Add(new Point(e.X, e.Y));
+
+                if (listap.Count() >= 2)
+                {
+                    
+                    Graphics gr = Graphics.FromImage(bmp);
+
+                    gr.DrawLines(new Pen(colork, rozmiar), listap.ToArray());
+                    Paint4d.Image = bmp;
+
+                }
+            }
+            
+            this.Text = $"{e.X} X{e.Y}";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                Paint4d.CreateGraphics().Clear(Color.White);
+                bmp = new Bitmap(Paint4d.Width, Paint4d.Height);
+                using (var g = Graphics.FromImage(bmp))
+                    g.Clear(Color.White);
+            
+            
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = colorDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                colork = new SolidBrush(colorDialog1.Color);
+            }
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            rozmiar = trackBar1.Value;
+        }
+        private void Paint4d_MouseUp(object sender, MouseEventArgs e)
+        {
+            listap.Clear();
+        }
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(Paint4d.Width, Paint4d.Height);
+            using (var g = Graphics.FromImage(bmp))
+                g.Clear(Color.White);
+            Graphics gr = Graphics.FromImage(bmp);
+            gr.Clear(Color.White);
+            timer1.Interval = 500;
+            timer1.Start();
+
+
+            var port = int.Parse(ConfigurationManager.AppSettings.Get("consumerPort"));
+            var client = new UdpClient(port);
+
+
+            while (true)
+            {
+                var data = await client.ReceiveAsync();
+                using (var ms = new MemoryStream(data.Buffer))
+                {
+                    StreamReader reader = new StreamReader(ms);
+                    char[] str = reader.ReadToEnd().ToCharArray();
+                    if (str[0] == 'k')
+                    {
+                        str[0] = ' ';
+                        listBox1.Items.Add("On:" + new string(str));
+
+                    }
+                    else
+                    {
+                        bmp = new Bitmap(ms);
+                        Paint4d.Image = bmp;
+                    }
+                        
+                }
+
+            }
+
+        }
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap bmpSave = (Bitmap)Paint4d.Image;
+                SaveFileDialog sfd = new SaveFileDialog();
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    bmpSave.Save(sfd.FileName, ImageFormat.Bmp);
+                }
+                saveFileDialog1.Filter = "*.jpg|*.jpg|*.bmp|*.bmp";
+                DialogResult dr = saveFileDialog1.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    bmp.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics gr = Graphics.FromImage(bmp);
+            gr.Clear(Color.White);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Filter = "*.jpg|*.jpg|*.bmp|*.bmp";
+                DialogResult dr = openFileDialog1.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    Bitmap b1 = new Bitmap(openFileDialog1.FileName);
+                    bmp = new Bitmap(b1, new Size(Paint4d.Width, Paint4d.Height));
+                    
+                    
+                    Paint4d.Image = bmp;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+            tabControl1.Top = this.ClientSize.Height - tabControl1.Height - 10;
+            Paint4d.Left = 0;
+            Paint4d.Top = 0;
+            Paint4d.Width = this.ClientSize.Width;
+            Paint4d.Height = this.ClientSize.Height - (this.ClientSize.Height - tabControl1.Top) - 10;
+        }
+        private void Paint4d_SizeChanged(object sender, EventArgs e)
+        {
+            Graphics gr = Graphics.FromImage(bmp);
+            gr.Clear(Color.White);
+        }
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            if (bmp.Width < Paint4d.Width || bmp.Height < Paint4d.Height)
+            {
+                Bitmap bmp2 = new Bitmap(bmp);
+                bmp = new Bitmap(Paint4d.Width, Paint4d.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                gr.Clear(Color.White);
+                for (int x = 0; x < Math.Min(bmp2.Width, bmp.Width); x++)
+                {
+                    for (int y = 0; y < Math.Min(bmp2.Height, bmp.Height); y++)
+                    {
+                        bmp.SetPixel(x, y, Color.FromArgb(bmp2.GetPixel(x, y).R, bmp.GetPixel(x, y).G, bmp2.GetPixel(x, y).B));
+                    }
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    int sredni = (bmp.GetPixel(x, y).R + bmp.GetPixel(x, y).G + bmp.GetPixel(x, y).B) / 3;
+                    bmp.SetPixel(x, y, Color.FromArgb(sredni, sredni, sredni));
+                }
+            }
+            Paint4d.Image = bmp;
+        }
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            for (int x = 1; x < bmp.Width - 1; x++)
+            {
+                for (int y = 1; y < bmp.Height - 1; y++)
+                {
+                    int sredni1 = (bmp.GetPixel(x + 1, y).R + bmp.GetPixel(x - 1, y).R + bmp.GetPixel(x, y + 1).R + bmp.GetPixel(x, y - 1).R) / 4;
+                    int sredni2 = (bmp.GetPixel(x + 1, y).G + bmp.GetPixel(x - 1, y).G + bmp.GetPixel(x, y + 1).G + bmp.GetPixel(x, y - 1).G) / 4;
+                    int sredni3 = (bmp.GetPixel(x + 1, y).B + bmp.GetPixel(x - 1, y).B + bmp.GetPixel(x, y + 1).B + bmp.GetPixel(x, y - 1).B) / 4;
+                    bmp.SetPixel(x, y, Color.FromArgb(sredni1, sredni2, sredni3));
+
+                }
+            }
+            Paint4d.Image = bmp;
+        }
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+           
+            bmp = new Bitmap(Paint4d.Image);
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = Convert.ToInt32(bmp.Height * 0.66); y < bmp.Height; y++)
+                {
+                    bmp.SetPixel(x, y, bmp.GetPixel(x, Convert.ToInt32(bmp.Height * 0.66) - (y - Convert.ToInt32(bmp.Height * 0.66))));
+                  
+                }
+            }
+            
+            Paint4d.Image = bmp;
+            
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var consummerIP = ConfigurationManager.AppSettings.Get("consumerIP");
+            var consummerPort = int.Parse(ConfigurationManager.AppSettings.Get("consumerPort"));
+            consumerEndPoint = new IPEndPoint(IPAddress.Parse(consummerIP), consummerPort);
+
+            var bmp1 = new Bitmap(bmp, Paint4d.Height, Paint4d.Width);
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    bmp1.Save(ms, ImageFormat.Jpeg);
+                    var bytes = ms.ToArray();
+                    udpClient.Send(bytes, bytes.Length, consumerEndPoint);
+                }
+
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.ToString()); }
+
+        }
+
+            private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void button10_Click_2(object sender, EventArgs e)
+        {
+            var consummerIP = ConfigurationManager.AppSettings.Get("consumerIP");
+            var consummerPort = int.Parse(ConfigurationManager.AppSettings.Get("consumerPort"));
+            consumerEndPoint = new IPEndPoint(IPAddress.Parse(consummerIP), consummerPort);
+
+            var str ="k"+ textBox1.Text;
+            try
+            {
+                byte[] byteArray = Encoding.ASCII.GetBytes(str);
+                using (var ms = new MemoryStream(byteArray))
+                {
+
+
+                    udpClient.Send(byteArray, byteArray.Length, consumerEndPoint);
+                    listBox1.Items.Add("JA: "+textBox1.Text);
+                    textBox1.Text = "";
+                }
+
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.ToString()); }
+        }
+    }  
+}
